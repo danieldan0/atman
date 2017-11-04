6b4b038061fc6a70d82f36c6d28fddc998b41a23
Daniel Nakushnov <daniel.nakushnov@gmail.com>
2017-11-04 13:18:43 +0200

    Revert "trying... (WIP)"
    This reverts commit bb78bf59b9c9617159b174e2f86d1dbfb0c4c74d.

--- main.lua
+++ main.lua
@@ -1,37 +1,26 @@
 moonshine = require 'lib/moonshine'
 
 function love.load()
-  win_w = 640
-  win_h = 480
-  love.window.setTitle("atman")
-  love.window.setMode(win_w, win_h, {resizable = false})
-  screenBuffer = love.graphics.newCanvas(win_w, win_h)
-  term_shader = love.graphics.newShader(readAll("assets/shaders/term/term.glsl"))
+  local frag = readAll("assets/shaders/fisheye/fisheye.frag")
+  local vert = readAll("assets/shaders/fisheye/fisheye.vert")
+  fisheye = moonshine.Effect {
+    name = "fisheye",
+    shader = love.graphics.newShader(frag, vert)
+  }
+  effect = moonshine(moonshine.effects.crt)
+  .chain(moonshine.effects.scanlines)
+  .chain(moonshine.effects.glow)
+  .chain(fisheye)
   font = love.graphics.newFont("assets/fonts/Press_Start_2P/PressStart2P-Regular.ttf", 16)
 end
 
-function love.update(dt)
-end
-
 function love.draw()
-  love.graphics.setCanvas(screenBuffer)
-  love.graphics.setColor(255, 255, 255)
-  love.graphics.print({{0, 255, 0, 255}, "Hello world!"}, 200, 200)
-  love.graphics.print({{0, 255, 0, 255}, "hmmm..... :Thonk:"}, 200, 216)
-  love.graphics.print({{0, 255, 0, 255}, "Привет, мир!"}, 200, 232)
-  love.graphics.setCanvas()
-
-  term_shader:send("iResolution", {win_w, win_h})
-  term_shader:send("iChannel", screenBuffer)
-  love.graphics.setShader(term_shader)
-  love.graphics.draw(screenBuffer, 0, 0)
-  love.graphics.setShader()
-end
-
-function love.keypressed(k)
-  if k == "escape" then
-    love.event.quit()
-  end
+  love.graphics.setFont(font)
+  effect(function()
+    love.graphics.print({{0, 255, 0, 255}, "Hello world!"}, 200, 200)
+    love.graphics.print({{0, 255, 0, 255}, "hmmm..... :Thonk:"}, 200, 216)
+    love.graphics.print({{0, 255, 0, 255}, "Привет, мир!"}, 200, 232)
+  end)
 end
 
 function readAll(file)
