9434e3f7a2b74f65fb8597855ef58c4bef087f37
Daniel Nakushnov <daniel.nakushnov@gmail.com>
2017-11-05 19:01:22 +0200

    Buttons.

--- main.lua
+++ main.lua
@@ -23,8 +23,6 @@ function love.update(dt)
   mouseXY = {mouseXY[1] * 1.15, mouseXY[2] * 1.15}
   mouseXY = {mouseXY[1] + (mouseXY[2] * mouseXY[2] * mouseXY[1] * 0.06),
              mouseXY[2] + (mouseXY[1] * mouseXY[1] * mouseXY[2] * 0.065)}
-  local mask = (1.0 - smoothstep(0.98,1.0,math.abs(mouseXY[1])))
-             * (1.0 - smoothstep(0.98,1.0,math.abs(mouseXY[2])));
   mouseXY = {(mouseXY[1] + 1.0) / 2.0, (mouseXY[2] + 1.0) / 2.0}
   mouseXY = {mouseXY[1] * 640, mouseXY[2] * 496 - 16}
 end
@@ -43,6 +41,18 @@ function love.draw()
       love.graphics.print({{0, 255, 0, 255}, "Привет, мир!"}, 0, 456)
       love.graphics.print({{0, 255, 0, 255}, "???"}, 0, 240)
       love.graphics.rectangle("fill", 540, 380, 100, 100)
+      -- buttons
+      love.graphics.setColor({255, 255, 0, 255})
+      love.graphics.rectangle("line", 320-50, 240-50, 100, 100, 10)
+      if inArea(mouseXY[1], mouseXY[2], 320-50, 240-50, 100, 100) then
+        love.graphics.rectangle("fill", 320-50, 240-50, 100, 100, 10)
+      end
+      love.graphics.setColor({0, 255, 255, 255})
+      love.graphics.rectangle("line", 320+50, 240+50, 100, 100, 10)
+      if inArea(love.mouse.getX(), love.mouse.getY(), 320+50, 240+50, 100, 100) then
+        love.graphics.rectangle("fill", 320+50, 240+50, 100, 100, 10)
+      end
+      -- cursors
       love.graphics.setColor({255, 0, 0, 255})
       love.graphics.rectangle("fill", mouseXY[1] - 5, mouseXY[2] - 5, 10, 10)
       love.graphics.setColor({0, 0, 255, 255})
@@ -67,7 +77,6 @@ function readAll(file)
   return content
 end
 
-function smoothstep(a, b, t)
-  t = t * t * t * (t * (t * 6 - 15) + 10)
-  return a + (b - a) * t
+function inArea(x, y, area_x, area_y, area_w, area_h)
+  return x >= area_x and x <= area_x + area_w and y >= area_y and y <= area_y + area_h
 end
