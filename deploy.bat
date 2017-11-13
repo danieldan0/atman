b60a591fadf52f3fbffb6cdeb549b0900bca4ec0
Daniel Nakushnov <daniel.nakushnov@gmail.com>
2017-11-13 17:28:43 +0200

    Stuff.

---
+++ deploy.bat
@@ -0,0 +1,3 @@
+butler push builds\universal danieldan0/atman:windows-osx-linux-love
+butler push builds\win32 danieldan0/atman:windows-32
+butler push builds\win64 danieldan0/atman:windows-64
--- src/main.lua
+++ src/main.lua
@@ -1,6 +1,9 @@
-moonshine = require 'lib/moonshine'
+local moonshine = require 'lib/moonshine'
+local utils = require 'utils'
+local handle_input = require 'input_handlers'
 
 function love.load()
+  -- Loading shaders
   s_scanlines = moonshine(moonshine.effects.scanlines)
   .chain(moonshine.effects.crt)
   s_scanlines.scanlines.thickness = 0.5
@@ -12,79 +15,82 @@ function love.load()
   s_screen.scanlines.thickness = 0.5
   s_screen.crt.scaleFactor = {1.15, 1.15}
 
+  -- Loading font
   font = love.graphics.newFont("assets/fonts/Press_Start_2P/PressStart2P-Regular.ttf", 24)
+  love.graphics.setFont(font)
+
+  -- Loading images
   frame = love.graphics.newImage("assets/images/screen-frame.png")
-  mouseXY = {0, 0}
-  tileXY = {0, 0}
-  playerXY = {0, 0}
+
+  -- Initialiazing game state.
+  game = {
+    playerXY = {0, 0},
+    user_input = {
+      keys = {},
+      mouseXY = {0, 0},
+      tileXY = {0, 0},
+      pressed_key = false
+    }
+  }
+  action = {}
 end
 
 function love.update(dt)
+  if game.user_input.pressed_key then
+    action = handle_input(game.user_input)
+    move = action['move']
+    exit = action['exit']
+    fullscreen = action['fullscreen']
+
+    if move then
+      dx, dy = unpack(move)
+      game.playerXY = {game.playerXY[1] + dx, game.playerXY[2] + dy}
+    end
+
+    if exit then
+      love.event.quit()
+    end
+
+    if fullscreen then
+      love.window.setFullscreen(not love.window.getFullscreen(), "exclusive")
+    end
+
+    game.user_input.pressed_key = false
+  end
 end
 
 function love.keypressed(key)
-  if key == "z" then
-    print(tileXY[1]..";"..tileXY[2])
-  end
+  game.user_input.keys[key] = true
+  game.user_input.pressed_key = true
+end
 
-  if key == "up" then
-    playerXY = {playerXY[1], playerXY[2] - 1}
-  elseif key == "down" then
-    playerXY = {playerXY[1], playerXY[2] + 1}
-  elseif key == "left" then
-    playerXY = {playerXY[1] - 1, playerXY[2]}
-  elseif key == "right" then
-    playerXY = {playerXY[1] + 1, playerXY[2]}
-  end
+function love.keyreleased(key)
+  game.user_input.keys[key] = nil
 end
 
 function love.mousemoved(x, y, dx, dy, istouch)
   -- Correcting mouse coordinates.
-  mouseXY = fisheye({x, y}, 640, 496, 1.15, 1.06, 1.065)
-  tileXY = {math.floor(mouseXY[1] / 24), math.floor(mouseXY[2] / 24)}
+  game.user_input.mouseXY = utils.fisheye({x, y}, 640, 496, 1.15, 1.06, 1.065)
+  game.user_input.tileXY = {math.floor(game.user_input.mouseXY[1] / 24), math.floor(game.user_input.mouseXY[2] / 24)}
 end
 
 function love.draw()
-  love.graphics.setFont(font)
-
   s_scanlines(function()
     love.graphics.setColor({5, 5, 5, 255})
     love.graphics.rectangle("fill", 0, 0, 640, 480)
   end)
 
   s_screen(function()
-    drawChar("@", playerXY[1], playerXY[2], {255, 0, 0, 255}, {0, 255, 0, 255})
-    if tileXY[1] == playerXY[1] and  tileXY[2] == playerXY[2] then
-      drawChar("@", playerXY[1], playerXY[2], {255, 127, 127, 255}, {127, 255, 127, 255})
+    drawChar("@", game.playerXY[1], game.playerXY[2], {255, 0, 0, 255}, {0, 255, 0, 255})
+    if game.user_input.tileXY[1] == game.playerXY[1] and game.user_input.tileXY[2] == game.playerXY[2] then
+      drawChar("@", game.playerXY[1], game.playerXY[2], {255, 127, 127, 255}, {127, 255, 127, 255})
     end
     -- cursors
     love.graphics.setColor({255, 0, 0, 255})
-    love.graphics.rectangle("fill", mouseXY[1] - 5, mouseXY[2] - 5, 10, 10)
+    love.graphics.rectangle("fill", game.user_input.mouseXY[1] - 5, game.user_input.mouseXY[2] - 5, 10, 10)
   end)
 
   love.graphics.draw(frame, 0, 0)
-end
-
-function readAll(file)
-  local f = io.open(file, "rb")
-  local content = f:read("*all")
-  f:close()
-  return content
-end
-
-function inArea(x, y, area_x, area_y, area_w, area_h)
-  return (((x >= area_x) and (x <= area_x + area_w)) and ((y >= area_y) and (y <= area_y + area_h)))
-end
-
-function fisheye(vec, w, h, scale, distortX, distortY)
-  result = {vec[1] / w, vec[2] / h}
-  result = {result[1] * 2.0 - 1.0, result[2] * 2.0 - 1.0}
-  result = {result[1] * scale, result[2] * scale}
-  result = {result[1] + (result[2] * result[2] * result[1] * (distortX - 1)),
-             result[2] + (result[1] * result[1] * result[2] * (distortY - 1))}
-  result = {(result[1] + 1.0) / 2.0, (result[2] + 1.0) / 2.0}
-  result = {result[1] * w, result[2] * h}
-  return result
 end
 
 function drawChar(char, x, y, color, bg)
