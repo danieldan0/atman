c04b7831f0fcb0f96fb944a5e7334472c64e2934
Daniel Nakushnov <daniel.nakushnov@gmail.com>
2017-11-30 22:15:17 +0200

    Fixed build

--- build.bat
+++ build.bat
@@ -7,5 +7,3 @@ del builds\win32\atman.exe builds\win64\atman.exe builds\universal\atman.love
 7z a -tzip ".\builds\universal\atman.love" ".\src\*"
 copy /b love\win32\love.exe+builds\universal\atman.love builds\win32\atman.exe
 copy /b love\win64\love.exe+builds\universal\atman.love builds\win64\atman.exe
-rd src\assets /s /q
-rd src\lib /s /q
